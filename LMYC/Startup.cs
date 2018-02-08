cc7421d7c5b689938fde89567c99440ac7f42bbe
Benjamin Hao <haomingkai@gmail.com>
2018-02-08 09:49:35 -0800

    Add project files.

---
+++ LMYC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMYC.Startup))]
+namespace LMYC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
